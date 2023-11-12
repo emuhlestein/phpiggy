9347099928b5e0df0920d016ed8b179efd32e0b7
Ed Muhlestein <emuhlestein@yahoo.com>
2023-11-12 06:26:31 -0700

    Dev php 1 (#1)
    * Add composer.json file
    
    * Add router
    
    * Add controller for home page

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
-include __DIR__ . "/../Framework/App.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
+
+// add routes
+$app->get('/', [HomeController::class, 'home']);
+
 
 return $app;
